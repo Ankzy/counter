ca935eaddaed8dc028cf5df6ae2ff2c947c4b8ed
unknown <ankzy33@mail.ru>
2020-12-22 17:11:26 +0300

    prod by ankz

--- counter/views.py
+++ counter/views.py
@@ -1,5 +1,4 @@
 from django.views.generic import TemplateView
-from counter.storage import counter
 from counter.management.commands.tgbot import Command
 import telebot
 
@@ -8,7 +7,11 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        data['counter'] = counter.inc()
+        if 'counter' in data:
+            data['counter'] = 2
+        else:
+            data['counter'] = 1
         a = Command()
         a.handle()
+
         return data
